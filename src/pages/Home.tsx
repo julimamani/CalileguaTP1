f97e4f6f2a4bd6f53d1f3e46c30795cb6dc09a23
julimamani <julietamamani47@gmail.com>
2024-10-05 23:59:39 -0300

    bases

---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+const Home = () => {
+    return (
+        <div className="pure-g">
+        <div className="pure-u-1 pure-u-md-1-2">
+            <h1>Hola, soy Juelieta Agustina Mamani Perez</h1>
+            <p>Desarrollador/a de software especializado/a en front-end, Analista y diseñadora </p>
+            <h2>Tecnologías que manejo:</h2>
+       
+            </div>        
+        <ul>
+          <li>Java, C++, SpringBoot</li>
+          <li>Angular</li>
+        </ul>
+        <h2>Aprendiendo:</h2>
+        <p>Curso de desarrollo Frontens en Calilegua</p>
+        <h2>Proyectos Destacados:</h2>
+        {/* Aquí puedes incluir componentes ProjectCard para mostrar proyectos */}
+      </div>
+     
+    );
+  };
+  
+  export default Home;
